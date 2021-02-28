3cc96d2b01f4176573eb4f9be72539e194fb1672
Amogh Wagh <amoghwagh@gmail.com>
2021-02-28 22:27:27 +0530

    Changed resumeData json link

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
 
   getResumeData() {
     $.ajax({
-      url: "/resumeData.json",
+      url: "https://amoghwagh.github.io/resume/resumeData.json",
       dataType: "json",
       cache: false,
       success: function (data) {
